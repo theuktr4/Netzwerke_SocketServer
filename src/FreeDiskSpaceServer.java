4c336c241d4596b5749b2a6d09f813c402e05bb0
hm-haerting <x7tas4EZ>
2020-10-31 17:14:30 +0100

    Exceptions, Methode

--- src/FreeDiskSpaceServer.java
+++ src/FreeDiskSpaceServer.java
@@ -3,65 +3,49 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.Buffer;
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class FreeDiskSpaceServer {
     private static final int PORT =4711;
+    public static final String CHARSET_NAME = "UTF8";
 
-    public FreeDiskSpaceServer(){
+    public FreeDiskSpaceServer() {
         try {
             ServerSocket server = new ServerSocket(PORT);
             System.out.println("Server running");
-            while(true) {
-                /*
-                Thread t = new Thread(() -> {
-                    try {
-                        Socket client = server.accept();
-                        System.out.println("new thread");
-                        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream(), "UTF8"));
-                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), "UTF8"));
-                        String line = in.readLine();
-                        Path path = Paths.get(line);
-                        long totalSpace = Files.getFileStore(path).getTotalSpace();
-                        long freeSpace = Files.getFileStore(path).getUnallocatedSpace();
-                        String res = String.format("Info for path %s %d bytes of %d free", line, freeSpace, totalSpace);
-                        System.out.printf("Sending: Info for path %s %d bytes of %d free %n", line, freeSpace, totalSpace);
+            while (true) {
+                try (Socket client = server.accept();) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream(), CHARSET_NAME));
+                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), CHARSET_NAME));
 
-                        out.write(res + "\n");
-                        out.newLine();
-                        out.flush();
-                    } catch (IOException cex) {
-                        System.out.println("Client disconnected");
-                    }
-                });
-                t.start();
-                 */
-                try {
-                    Socket client = server.accept();
-                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream(), "UTF8"));
-                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), "UTF8"));
                     String line = in.readLine();
-                    Path path = Paths.get(line);
-                    long totalSpace = Files.getFileStore(path).getTotalSpace();
-                    long freeSpace = Files.getFileStore(path).getUnallocatedSpace();
-                    String res = String.format("Info for path %s %d bytes of %d free", line, freeSpace, totalSpace);
-                    System.out.printf("Sending: Info for path %s %d bytes of %d free %n", line, freeSpace, totalSpace);
-
+                    System.out.println("Input" + line);
+                    String res ="";
+                    try {
+                        res = getFreeDiskSpace(line);
+                    } catch (IOException pathfail){
+                        res = "Ungültiger Pfad";
+                    }
                     out.write(res + "\n");
                     out.newLine();
                     out.flush();
-                } catch (IOException cex) {
-                    System.out.println("Client disconnected");
+
+                } catch (IOException ex) {
+                    System.out.println(ex);
                 }
             }
         } catch (IOException e) {
             System.out.println("Server closed");
         }
-
     }
+
+        String getFreeDiskSpace(String pathString) throws IOException {
+            Path path = Paths.get(pathString);
+            long totalSpace = Files.getFileStore(path).getTotalSpace();
+            long freeSpace = Files.getFileStore(path).getUnallocatedSpace();
+            return String.format("Info for path %s: %d bytes of %d free", pathString, freeSpace, totalSpace);
+        }
+
     public static void main(String[] args) {
         new FreeDiskSpaceServer();
     }
